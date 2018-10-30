747c976f3ee3791154acd3f9bfe87fb22c64f5d4
Calev Chandra <calev.chandra@vodafone.com>
2018-10-30 23:02:28 +1200

    calev

---
+++ src/components/counter.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
+  /*renderTag() {
+    if (this.state.tags.length === 0) return <p>No tags yet</p>;
+    else {
+      return (
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+    }
+  }*/
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            className="btn btn-secondary btn-sm"
+            onClick={() => this.props.onIncrement(this.props.counter)}
+          >
+            Increment
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
+        </div>
+        {/*{this.state.tags.length === 0 && <p>No tags created</p>}
+        {this.renderTag()}
+    */}
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
